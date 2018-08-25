5a686a4e9f6f9f3132f5ceaa6c89d2f541751a89
James Sieu <jamessieu@gmail.com>
2018-08-24 20:56:35 -0700

    basic functionality of camera added to the client.  Running the app will start the camera.  Tapping anywhere will capture the image.  Save and upload of image to the server have not been implemented yet.

--- src/client/App.js
+++ src/client/App.js
@@ -1,13 +1,85 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, ImageBackground } from 'react-native'
+// import Autoshoot from './Autoshoot.js'
+
+import { Camera, Permissions } from 'expo';
+
+
+//insert SERVER URL
+const SERVER_URL = '';
+
+
+class Autoshoot extends React.Component {
+  state = {
+    photo: null
+  }
+  takePicture = ()=> {
+    this.camera.takePictureAsync({
+      quality: 0.1,
+      base64: true,
+      exif: false
+    }).then(photo => {
+      this.setState({photo});
+    })
+  }
+
+  uploadPicture = () => {
+    return fetch(SERVER_URL, {
+      body: JSON.stringify({
+        image: this.state.photo.base64
+      }),
+      headers: {
+        'content-type': 'application/json'
+      },
+      method: 'POST'
+      })
+      .then(response => response.json())
+  }
+  render() {
+     const {photo} = this.state;
+
+    return (
+      <View style={{ flex: 1, width: '100%' }}>
+      {photo ? (
+        <ImageBackground style={{flex:1}} source={{uri:photo.uri}}/>):
+      (
+        <Camera 
+          style = {{flex: 1}}
+          type={Camera.Constants.Type.back}
+          ref={cam => this.camera = cam}>
+          <TouchableOpacity style={{flex:1}} onPress={this.takePicture}/>
+        </Camera>  
+      )} 
+      </View>
+    );
+  }
+}
+
 
 export default class App extends React.Component {
+  //initialize state
+  state = {
+    cameraPermission: null
+  };
+
+  componentDidMount() {
+    Permissions.askAsync(Permissions.CAMERA)
+      .then(({ status }) =>
+        this.setState({
+          cameraPermission: status === 'granted'
+          // photo: null
+        })
+      );
+  }
+
   render() {
+    const { cameraPermission } = this.state;
+
+    //Render one of 3 things depending on permissions
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        {cameraPermission === null ? (<Text>Waiting for permission...</Text>) : cameraPermission === false}
+        {cameraPermission === false ? (<Text>Permission denied</Text>) : (<Autoshoot/>)}
       </View>
     );
   }
@@ -21,3 +93,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
