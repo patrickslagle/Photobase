3ad15005782619b13bf2baf17fe7004450c53bf9
andwong91 <andwong91@gmail.com>
2018-08-28 21:50:29 -0700

    Created postgres server and integrated it with the front end components. Successfully tested retrieving data from the db

--- website/src/server/router.js
+++ website/src/server/router.js
@@ -1,26 +1,51 @@
 const path = require('path');
-const mysql = require('mysql');
-const mysqlDB = mysql.createConnection({
-  host     : '*',
-  user     : 'root',
-  password : process.env.PASSWORD,
-  database : process.env.DATABASE,
-  socketPath: process.env.SOCKETPATH,
-  connectTimeout: 10000
-});
-mysqlDB.connect();
+const db = require('./db.js');
+
 module.exports = (app) => {
-  console.log('im in router????');
-  //console.log(mysqlDB);
-  app.get('/users', (req, res, next) => {
-    console.log('im in router!');
-    console.log(mysqlDB);
-    mysqlDB.query('SELECT * FROM users;', (err, rows, fields) => {
-      if (err) throw err;
-      console.log('rows', rows[0].username);
-      // console.log('The user is: ', rows[0].username);
-      res.send(rows);
-    })
-    mysqlDB.end()
+  app.get('/images', (req, res, next) => {
+    db.query('SELECT uri FROM images;')
+      .then((data) => {
+        res.send(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
   });
-};
+  app.get('/users', (req, res, next) => {
+    db.query('SELECT username FROM users;')
+      .then((data) => {
+        res.send(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  });
+}
+
+// mySQL stuff
+
+// const mysql = require('mysql');
+// const mysqlDB = mysql.createConnection({
+//   host     : '*',
+//   user     : 'root',
+//   password : process.env.PASSWORD,
+//   database : process.env.DATABASE,
+//   socketPath: process.env.SOCKETPATH,
+//   connectTimeout: 10000
+// });
+// mysqlDB.connect();
+// module.exports = (app) => {
+//   console.log('im in router????');
+//   //console.log(mysqlDB);
+//   app.get('/users', (req, res, next) => {
+//     console.log('im in router!');
+//     console.log(mysqlDB);
+//     mysqlDB.query('SELECT * FROM users;', (err, rows, fields) => {
+//       if (err) throw err;
+//       console.log('rows', rows[0].username);
+//       // console.log('The user is: ', rows[0].username);
+//       res.send(rows);
+//     })
+//     mysqlDB.end()
+//   });
+// };
--- website/src/server/server.js
+++ website/src/server/server.js
@@ -1,7 +1,6 @@
 
 require('dotenv').config();
 
-
 const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -12,49 +11,15 @@ const PORT = 3000;
 
 const router = require('./router.js');
 
-
-//test connection, will properly handle the route through app.get or some other route:
-app.use('/', express.static(path.join(__dirname, './../../dist')));
-
 //console.log('env', process.env.PASSWORD);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
+//test connection, will properly handle the route through app.get or some other route:
+app.use('/', express.static(path.join(__dirname, './../../dist')));
 router(app);
-
-const router = require('./router.js');
-
-// localhost port number
-const PORT = 3000;
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// allow access from other servers
-app.use(cors());
-
-// define routes before error handlers
-app.use(express.static(path.join(__dirname, './../../dist')));
-router(app);
-
-
-const router = require('./router.js');
-
-// localhost port number
-const PORT = 3000;
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// allow access from other servers
-app.use(cors());
-
-// define routes before error handlers
-app.use(express.static(path.join(__dirname, './../../dist')));
-router(app);
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
