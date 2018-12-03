85d6fbc5c76d1485595919a42bfb91a33876e750
Patrick Slagle <slaglebagel17@gmail.com>
2018-12-03 11:54:21 -0800

    linted component, removed console.logs

--- website/src/client/App.jsx
+++ website/src/client/App.jsx
@@ -1,49 +1,43 @@
 import React from 'react';
-const axios = require('axios');
+import axios from 'axios';
 import Search from './components/Search';
 import Header from './components/Header';
 import Display from './components/Display';
-import "./style.css"
+import "./style.css";
 
 
 class App extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            images: [],
-            searchInput: '',
-        }
-        this.handleChange=this.handleChange.bind(this);
-        this.handleSubmit=this.handleSubmit.bind(this)
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      images: [],
+      searchInput: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-    componentDidMount() {
-        axios.get('/images')
-            .then((images) => {
-                console.log('what is images.data?', images.data)
-                this.setState({ images: images.data });
-            })
-            .catch((err) => {
-                console.log(err);
-            })
-    }
-    handleChange(event) {
-        this.setState({value: event.target.value});
-    }
-    
-    handleSubmit(event) {
-        event.preventDefault();
-    }
+  componentDidMount() {
+    axios.get('/images')
+      .then((images) => {
+        this.setState({ images: images.data });
+      })
+      .catch(err => console.log(err));
+  }
 
-    render() {
-        return (
-            <div>
-                <Header />
-                <Search handleChange={this.handleChange} searchInput={this.state.searchInput}/>
-                <Display images={this.state.images}/>
-            </div>
-        )
-    }
+  handleChange(event) {
+    this.setState({ searchInput: event.target.value });
+  }
 
-} 
+  render() {
+    const { searchInput, images } = this.state;
+    return (
+      <div>
+        <Header />
+        <Search handleChange={this.handleChange} searchInput={searchInput} />
+        <Display images={images} />
+      </div>
+    );
+  }
+}
+
 export default App;
