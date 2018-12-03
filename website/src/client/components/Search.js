6045e7808cf944bc743d058d6232576acfe23684
Patrick Slagle <slaglebagel17@gmail.com>
2018-12-03 11:52:58 -0800

    removing unnecessary code

--- website/src/client/components/Search.js
+++ website/src/client/components/Search.js
@@ -4,13 +4,15 @@ import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 
 
-const Search = (props) => {
-    console.log(props)
-    return (
-        <div className="search-container">
-            <input type="text" className="search" onChange={props.handleChange} value={props.searchInput}/>
-            <Button className="search">Search</Button>
-        </div>
-    )
-}
+const Search = ({ handleChange }) => (
+  <div className="search-container">
+    <input 
+      type="text" 
+      className="search" 
+      onChange={handleChange} 
+    />
+    <Button className="search">Search</Button>
+  </div>
+);
+
 export default Search;
